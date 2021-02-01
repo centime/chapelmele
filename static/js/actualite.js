ebaea012188705a7f8c83bff3459df97d5bba0a6
centime <quelques.centimes@gmail.com>
2021-02-01 22:49:19 +0100

    actualite + new content pics

---
+++ static/js/actualite.js
@@ -0,0 +1,15 @@
+async function fetchPosts(){
+  template = $('#post-template').html();
+  $posts = await $.get('https://chapelmele.github.io/website/actualites.json');
+
+  for (var i=0; i<$posts.length; i++){
+    var $bloc = $(template);
+    $bloc.find('h2').html('<strong>'+$posts[i].title+'</strong><br/>'+ $posts[i].date);
+    $bloc.find('.bloc-text p').html($posts[i].content);
+    $bloc.find('.bloc-pic img').attr('src', 'https://chapelmele.github.io/website/actu-imgs/'+$posts[i].picture);
+    $('#feed').append($bloc);
+    (i%2 == 0) ? $bloc.toggleClass('bloc-img-right bloc-img-left') : 0;
+  }
+}
+
+$(document).ready(fetchPosts);
--- static/js/progCalendar.js
+++ static/js/progCalendar.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
       initialView: 'dayGridMonth',
-      events: '/chapelmele/static/js/test.json',
+      events: '/static/js/test.json',
       locale: 'fr'
     });
     calendar.render();
