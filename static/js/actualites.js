8c4c0617f14ff3117a2addbb99f4134e4f29922f
centime <quelques.centimes@gmail.com>
2021-02-07 11:50:41 +0100

    [fix] zepto broke /actualites/, fixed it back

--- static/js/actualites.js
+++ static/js/actualites.js
@@ -1,13 +1,14 @@
 async function fetchPosts(){
-  template = $('#post-template').html();
-  $posts = await $.get('//data.chapelmele.com/actualites.json');
-
-  for (var i=0; i<$posts.length; i++){
+  const template = $('#post-template').html();
+  const response = await fetch("//data.chapelmele.com/actualites.json");
+  const posts = await response.json();
+  
+  for (var i=0; i<posts.length; i++){
     var $bloc = $(template);
-    $bloc.find('h2').html('<strong>'+$posts[i].title+'</strong><br/>'+ $posts[i].date);
-    $bloc.find('.bloc-text p').html($posts[i].content);
-    if ($posts[i].picture) {
-      $bloc.find('.bloc-pic img').attr('src', '//data.chapelmele.com/actu-imgs/'+$posts[i].picture);
+    $bloc.find('h2').html('<strong>'+posts[i].title+'</strong><br/>'+ posts[i].date);
+    $bloc.find('.bloc-text p').html(posts[i].content);
+    if (posts[i].picture) {
+      $bloc.find('.bloc-pic img').attr('src', '//data.chapelmele.com/actu-imgs/'+posts[i].picture);
     }
     $('#feed').append($bloc);
     (i%2 == 0) ? $bloc.toggleClass('bloc-img-right bloc-img-left') : 0;
@@ -15,3 +16,4 @@ async function fetchPosts(){
 }
 
 $(document).ready(fetchPosts);
+
