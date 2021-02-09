094d96c8c179729609900f86e4797feb6fd41834
centime <quelques.centimes@gmail.com>
2021-02-09 13:49:29 +0100

    [cleanup] general-defer.js, .gitignore

--- static/js/deferIframes.js
+++
@@ -1,9 +0,0 @@
-function deferIframe() {
-  var iframeElem = document.getElementsByTagName('iframe');
-  for ( var i = 0; i < iframeElem.length; i++ ) {
-    if(iframeElem[i].getAttribute('data-src')) {
-      iframeElem[i].setAttribute('src',iframeElem[i].getAttribute('data-src'));
-    } 
-  } 
-}
-window.onload = deferIframe;
