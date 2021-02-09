74ca9951e0c1f1d477c8151f005bf4b0ba79ce87
centime <quelques.centimes@gmail.com>
2021-02-09 15:12:39 +0100

    [cleanup] js imports & defer & merges

--- assets/js/agenda.js
+++ assets/js/agenda.js
@@ -14330,3 +14330,67 @@ var FullCalendar = (function (exports) {
     return exports;
 
 }({}));
+
+
+function displayAnchoredEvent(){
+	// todo: history.pushState ?
+	// todo: update metas for direct linking
+	$('.event-desc').hide(); 
+	var target = location.hash;
+	var $evs = $('.fc-event');
+	$evs.find('[event-id="'+target+'"]').show(); 
+}
+
+function init(){
+	var $calendar = $('#calendar')[0];
+	// todo: fix: global for dev purposes only
+    calendar = new FullCalendar.Calendar($calendar, {
+      initialView: 'dayGridMonth',
+      locale: 'fr',
+	  eventDidMount: function(ev) {
+        console.log(ev.el);
+        console.log(ev.event.extendedProps.description);
+        $(ev.el).append('<div class="event-desc" event-id="'+ev.event.url+'" style="display:none">'+ev.event.extendedProps.description+'</div>');
+      }
+    });
+    calendar.render();
+
+    async function update(calendar){
+		var events = await $.get('/assets/js/test.json');
+		calendar.addEventSource(events);
+		displayAnchoredEvent();
+		window.onhashchange = displayAnchoredEvent;
+	}
+    update(calendar);
+}
+
+$(document).ready(init);
+FullCalendar.globalLocales.push(function () {
+  'use strict';
+
+  var fr = {
+    code: 'fr',
+    week: {
+      dow: 1, // Monday is the first day of the week.
+      doy: 4, // The week that contains Jan 4th is the first week of the year.
+    },
+    buttonText: {
+      prev: 'Précédent',
+      next: 'Suivant',
+      today: "Aujourd'hui",
+      year: 'Année',
+      month: 'Mois',
+      week: 'Semaine',
+      day: 'Jour',
+      list: 'Planning',
+    },
+    weekText: 'Sem.',
+    allDayText: 'Toute la journée',
+    moreLinkText: 'en plus',
+    noEventsText: 'Aucun événement à afficher',
+  };
+
+  return fr;
+
+}());
+
