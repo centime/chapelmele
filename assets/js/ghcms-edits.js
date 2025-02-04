722a73951d8bd46c5b83c48da36cedaad534320f
centime <centime@users.noreply.github.com>
2025-02-04 17:03:18 +0100

    new octokit source test

--- assets/js/ghcms-edits.js
+++ assets/js/ghcms-edits.js
@@ -435,7 +435,9 @@ const pageLocation = document.location.pathname + document.location.search;
 var edits, editsSha, editsAsCommited;
 
 
-import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
+// import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
+// https://github.com/octokit/octokit.js/#usage
+import { Octokit } from "https://esm.sh/octokit";
 const octokit = new Octokit({ auth: ghCMSCredentials.token });
 
 // todo: restrict hns to h3s
